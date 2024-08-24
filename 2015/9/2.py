aec26f754572a64510e8a15f07e7c24fc9293857
nibley <5225323+nibley@users.noreply.github.com>
2024-08-23 18:19:09 -0600

    2015 9

---
+++ 2015/9/2.py
@@ -0,0 +1,52 @@
+from itertools import permutations
+from functools import cache
+from math import inf
+
+edges = {}
+def parse_edge(line):
+    left_side, cost = line.split(' = ')
+    source, destination = left_side.split(' to ')
+    edges.setdefault(source, {})
+    edges[source][destination] = int(cost)
+
+@cache
+def get_edge(source, destination):
+    if source in edges and destination in edges[source]:
+        return edges[source][destination]
+    
+    return edges[destination][source]
+    
+def path_cost(path):
+    total_cost = 0
+    for i, source in enumerate(path[:-1]):
+        destination = path[i + 1]
+        total_cost += get_edge(source, destination)
+    return total_cost
+
+while True:
+    try:
+        line = input()
+    except EOFError:
+        break
+    
+    parse_edge(line)
+
+highest_path = ()
+highest_cost = -inf
+
+nodes = set()
+for k, v in edges.items():
+    nodes.add(k)
+    for k2 in v.keys():
+        nodes.add(k2)
+paths = permutations(nodes)
+for path in paths:
+    cost = path_cost(path)
+    if cost > highest_cost:
+        print(f'Found more expensive path {cost}')
+        highest_path = path
+        highest_cost = cost
+
+print()
+print(' -> '.join(highest_path))
+print(highest_cost)
