79cbe415f3490943c48c4f2b05d7fc96f673361a
nibley <5225323+nibley@users.noreply.github.com>
2024-08-26 22:07:58 -0600

    2016 6

---
+++ 2016/6/2.py
@@ -0,0 +1,17 @@
+from collections import defaultdict
+
+frequences = defaultdict(lambda: defaultdict(lambda: 0))
+while True:
+    try:
+        line = input()
+    except EOFError:
+        break
+    
+    for i, char in enumerate(line):
+        frequences[i][char] += 1
+
+message = ''
+for i, chars in sorted(frequences.items(), key=lambda item: item[0]):
+    message += sorted(chars.items(), key=lambda item: item[1])[0][0]
+
+print(message)
