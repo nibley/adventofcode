b3f7e8eb74e7f53d287e815b1db060e320ad8366
nibley <5225323+nibley@users.noreply.github.com>
2024-08-28 10:14:12 -0600

    2016 16

---
+++ 2016/16/2.py
@@ -0,0 +1,26 @@
+def dragon(data):
+    mirror = data[::-1]
+    mirror = mirror.replace('1', '_')
+    mirror = mirror.replace('0', '1')
+    mirror = mirror.replace('_', '0')
+    return f'{data}0{mirror}'
+
+def check(data):
+    result = ''
+    for i in range(0, len(data), 2):
+        result += '1' if data[i] == data[i + 1] else '0'
+    
+    if len(result) % 2:
+        return result
+        
+    return check(result)
+
+data = input()
+disk_length = 35651584
+
+while len(data) < disk_length:
+    data = dragon(data)
+data = data[:disk_length]
+
+checksum = check(data)
+print(checksum)
