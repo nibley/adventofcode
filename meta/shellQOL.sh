eacf6e8549a2ff85fb8bc79edf0bd378ed677df3
nibley <5225323+nibley@users.noreply.github.com>
2024-09-03 04:04:25 -0600

    update shell QOL and add a template.py file

--- meta/shellQOL.sh
+++ meta/shellQOL.sh
@@ -3,3 +3,4 @@ alias paste='cat /dev/clipboard >> input.txt'
 alias python="$HOME/AppData/Local/Programs/Python/Python312/python.exe"
 alias pythonrepl="winpty $HOME/AppData/Local/Programs/Python/Python312/python.exe"
 alias copy2='cat 1.py >> 2.py'
+alias template='cat ../../meta/template.py >> 1.py'
