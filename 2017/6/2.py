ac6c71bdb00dbcfe761cd8c99fa52289e8cc1e34
nibley <5225323+nibley@users.noreply.github.com>
2024-09-06 08:14:19 -0600

    2017 6

---
+++ 2017/6/2.py
@@ -0,0 +1,30 @@
+raw = input()
+banks = list(int(piece) for piece in raw.split('\t'))
+num_banks = len(banks)
+states = []
+
+cycles_completed = 0
+while True:
+    current_state = tuple(banks)
+    if current_state in states:
+        break
+    states.append(current_state)
+
+    fullest_bank, fullest_contents = sorted(enumerate(banks), \
+        key=lambda item: (item[1], -1 * item[0])
+    )[-1]
+    banks[fullest_bank] = 0
+    share_per_bank = fullest_contents // num_banks
+    for i in range(num_banks):
+        banks[i] += share_per_bank
+
+    remainder = fullest_contents % num_banks
+    remainder_recipients = [(fullest_bank + i) % num_banks \
+        for i in range(1, remainder + 1)
+    ]
+    for i in remainder_recipients:
+        banks[i] += 1
+
+    cycles_completed += 1
+
+print(cycles_completed - states.index(current_state))
