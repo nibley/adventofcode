2fd98eebf2268cc9fc6843718cbf2aea991235d3
nibley <5225323+nibley@users.noreply.github.com>
2024-09-13 23:54:07 -0600

    2015 5 (part 2, which had been off by one)

--- 2015/5/2.py
+++ 2015/5/2.py
@@ -1,44 +1,36 @@
-total_nice = 0
-
-debug_double = ''
-debug_gap = ''
-
 def pred_double_pair(line):
     pairs = set()
-    for i, c in enumerate(line[:-1]):
-        pair = line[i] + line[i+1]
-        if pair in pairs:
-            # print(' ', pair, line[i-1:i+1])
-            if pair != line[i-1:i+1]:
-                # print(line, 'doubles', pair)
-                global debug_double
-                debug_double = pair
-                return True
-        pairs.add(pair)
+    for i, char in enumerate(line[:-1]):
+        pair = char + line[i + 1]
+
+        if pair not in pairs:
+            pairs.add(pair)
+            continue
+
+        if (
+            pair != line[i - 1 : i + 1]
+            or pair == line[i - 2 : i]
+        ):
+            return True
+
     return False
 
 def pred_gap_repeat(line):
-    for i, c in enumerate(line[:-2]):
-        if c == line[i + 2]:
-            # print(line, 'gap', line[i:i+3])
-            global debug_gap
-            debug_gap = line[i:i+3]
+    for i, char in enumerate(line[:-2]):
+        if char == line[i + 2]:
             return True
+
     return False
 
 preds = [pred_double_pair, pred_gap_repeat]
-def is_nice(line):
-    return all(pred(line) for pred in preds)
-
+total_nice = 0
 while True:
     try:
         line = input()
     except EOFError:
         break
-    
-    good = is_nice(line)
-    if good:
+
+    if all(pred(line) for pred in preds):
         total_nice += 1
-        print(f'{total_nice}\t{line}\t{debug_double}\t{debug_gap}\n')
 
 print(total_nice)
--- README.md
+++ README.md
@@ -3,7 +3,6 @@
 All of my Advent of Code solutions, in Python for now.
 
 Skipped problems
-- 2015 5 (part 2 - off by one)
 - 2015 19 (part 2)
 - 2015 22
 - 2016 11
