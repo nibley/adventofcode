02da3d138a52c87d488f32d3a1a8221b6ac5c09c
nibley <5225323+nibley@users.noreply.github.com>
2024-09-15 01:52:32 -0600

    2017 10

---
+++ 2017/10/1.py
@@ -0,0 +1,29 @@
+raw = input()
+knot_lengths = map(int, raw.split(','))
+
+rope_size = 256
+rope = [i for i in range(rope_size)]
+
+position = 0
+skip_size = 0
+for knot_length in knot_lengths:
+    knot_region_main = slice(position, min(rope_size, position + knot_length))
+    wrap_length = position + knot_length - rope_size
+
+    if wrap_length < 1:
+        rope[knot_region_main] = reversed(rope[knot_region_main])
+    else:
+        knot_region_wrap = slice(0, wrap_length)
+        knot_region_reversed = ( \
+            rope[knot_region_main] + rope[knot_region_wrap] \
+        )[::-1]
+
+        rope[ : knot_region_wrap.stop] = \
+            knot_region_reversed[-1 * knot_region_wrap.stop : ]
+        rope[position : ] = \
+            knot_region_reversed[ : -1 * knot_region_wrap.stop]
+
+    position = (position + knot_length + skip_size) % rope_size
+    skip_size += 1
+
+print(rope[0] * rope[1])
