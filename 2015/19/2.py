13268b9f26a98daef24839d9c0a5270a07a63f73
nibley <5225323+nibley@users.noreply.github.com>
2024-09-15 04:49:47 -0600

    2015 19 (part 2 which had been skipped)

--- 2015/19/2.py
+++ 2015/19/2.py
@@ -1,83 +1,49 @@
-goal_molecule = ''
-goal_molecule_length = 0
-final_line_next = False
-replacements = {}
-graph = {}
-crawl_queue = []
-skipped = set()
+import random
 
-total_edges = 0
-def add_edge(start, end):
-    if len(end) > goal_molecule_length:
-        # print('LONG')
-        return False
-
-    graph.setdefault(start, [])
-    graph[start].append(end)
-    global total_edges
-    total_edges += 1
-    return True
-    # print(f'{total_edges} edges')
-
-def mutate_molecule(molecule):
-    # if molecule in graph or molecule in skipped:
-    
-    # if molecule in graph:
-        # print('REDUNDANT')
-        # return
-    
-    # if len(molecule) >= goal_molecule_length:
-    #     print('LONG', len(molecule), molecule)
-    #     skipped.add(molecule)
-    #     return
-
-    start_indices = {}
-    for start in starts:
-        indices = [i for i in range(len(molecule) - len(start) + 1) if molecule[i : i + len(start)] == start]
-        start_indices[start] = indices
-
-    for start in starts:
-        for start_index in start_indices[start]:
-            for end in replacements[start]:
-                before_start = molecule[:start_index]
-                after_start = molecule[start_index + len(start):]
-                new_molecule = f'{before_start}{end}{after_start}'
-                
-                added_edge = add_edge(molecule, new_molecule)
-                if added_edge and new_molecule not in graph:
-                    # if len(new_molecule) <= goal_molecule_length:
-                        # mutate_molecule(new_molecule)
-                        crawl_queue.append(new_molecule)
-
-
+molecule = None
+electron_direct_products = set()
+replacements = []
 while True:
     try:
         line = input()
-        if final_line_next:
-            goal_molecule = line
-            goal_molecule_length = len(goal_molecule)
-        elif line == '':
-            final_line_next = True
-        else:
-            start, end = line.split(' => ')
-            replacements.setdefault(start, [])
-            replacements[start].append(end)
     except EOFError:
         break
 
-starts = replacements.keys()
+    if not line:
+        molecule = input()
+        continue
 
-mutate_molecule('e')
+    end, start = line.split(' => ')
+    if end == 'e':
+        electron_direct_products.add(start)
+    else:
+        replacements.append( (start, end) )
 
-# prev_total_edges = 0
+def stochastic_approach(start_molecule):
+    steps = 0
+    molecule = start_molecule
+    while True:
+        old_molecule = molecule
+        molecule_changed = False
+        for _ in range(TRIES_BEFORE_GIVING_UP):
+            replacement = random.choice(replacements)
+            start, end = replacement
 
-while len(crawl_queue):
-    crawl_molecule = crawl_queue.pop()
-    mutate_molecule(crawl_molecule)
-    
-    print(total_edges)
-    
-    # print(total_edges, total_edges - prev_total_edges)
-    # prev_total_edges = total_edges
+            num_matches = molecule.count(start)
+            if num_matches:
+                molecule_changed = True
+                molecule = molecule.replace(start, end)
+                steps += num_matches
 
-# print(total_edges)
+        if not molecule_changed:
+            return None
+
+        if molecule in electron_direct_products:
+            return steps + 1
+
+TRIES_BEFORE_GIVING_UP = 40 # seems to work best
+steps = None
+while steps is None:
+    steps = stochastic_approach(molecule)
+
+print(steps)
--- README.md
+++ README.md
@@ -3,7 +3,6 @@
 All of my Advent of Code solutions, in Python for now.
 
 Skipped problems
-- 2015 19 (part 2)
 - 2015 22
 - 2016 11
 - 2016 19 (part 2)
@@ -13,3 +12,6 @@ Skipped problems
 - 2016 24
 - 2017 3 (part 2)
 
+Other problems to revisit
+- 2015 19 (part 2)
+Seems to have an unusually variable input generation. My input cannot be solved by a simple approach almost like part 1's, but many people's, including a friend's and those of several bloggers, can. I landed on a stochastic approach but a pathfinding approach would be fun to revisit (as long as it had ways to pare down the combinatorial explosion that I experienced with a naive approach even when starting with the medicine and working towards shorter and shorter molecules).
