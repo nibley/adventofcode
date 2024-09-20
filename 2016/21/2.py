61866b49d61873fcd5f17e34637b372099e9e923
nibley <5225323+nibley@users.noreply.github.com>
2024-09-19 20:20:26 -0600

    2016 21 (part 2 which had been skipped)
    note that unscrambling is impossible
    for most len(password), but not the one
    we are given, 8

--- 2016/21/2.py
+++ 2016/21/2.py
@@ -50,32 +50,21 @@ def reverse_operation(password, operation):
             password.index(first_letter), \
             password.index(second_letter)))
     elif operation_type == 'rotate letter':
-        [letter] = operation_args
-
-        if password != 'decab':
-            print('  (set to ecabd)')
-            password = 'ecabd'
-
-        # right instead of left because 'rotate' operation is backwards
-        left_by_one = reverse_operation(password, ('rotate', 'right', 1))
+        letter, *_ = operation_args
+        index = password.index(letter)
         
-        '''
-        ecabd
-        cabde
-        abdec
-        '''
+        for starting_index in range(len(password)):
+            resulting_index = (2 * starting_index + 1) % len(password)
+            if starting_index >= 4:
+                resulting_index += 1
+            if resulting_index == index:
+                # this is the first position for letter
+                # that would scramble into password
+                # note that unscrambling is impossible for
+                # most len(password), but not the one we are given
+                break
 
-        steps = left_by_one.index(letter)
-        # steps = left_by_one.index(letter) - 1
-        if steps >= 4:
-            steps += 1
-        steps -= 1
-        print(f'  leftbyone {left_by_one} {steps} {steps % len(password)}')
-
-        # right instead of left because 'rotate' operation is backwards
-        result = reverse_operation(left_by_one, ('rotate', 'right', steps))
-        print(f'  result {result}')
-        return result
+        return reverse_operation(password, ('rotate', 'right', index - starting_index))
     elif operation_type == 'rotate':
         direction, steps = operation_args
         offset = steps * (1 if direction == 'right' else -1)
@@ -104,46 +93,8 @@ def reverse_operation(password, operation):
             source_letter + \
             password_without_source_letter[destination_index:]
 
-'''
-abcde
----
-ebcda
-edcba
-abcde
-bcdea
-bdeac
-abdec
-ecabd
-
-decab
-'''
-
-'''
-decab
----
-ecabd
-cabde
-
-
-ecabd
-'''
-
-# password = 'fbgdceah'
-password = 'decab'
-n = 2
-# n = 1
-
-for operation in operations[:n]:
-# for operation in reversed(operations):
-# for operation in list(reversed(operations))[:n]:
-    print(operation)
-    print(password)
+password = 'fbgdceah'
+for operation in reversed(operations):
     password = reverse_operation(password, operation)
-    print(f'  {password}')
-    print()
 
-print()
-print(f'have {password}')
-print('---')
-print(f'want {'ecabd'}')
-print(f'want {'abdec'}')
+print(password)
