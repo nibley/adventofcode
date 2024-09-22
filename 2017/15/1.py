d1ab955067be66750434c84c89c8c5cc7383cdd7
nibley <5225323+nibley@users.noreply.github.com>
2024-09-22 01:53:00 -0600

    2017 15

---
+++ 2017/15/1.py
@@ -0,0 +1,22 @@
+divisor = 2147483647
+bitmask = 2 ** 16 - 1
+def generator(initial, multiplier):
+    value = initial
+    while True:
+        value = (value * multiplier) % divisor
+        yield value & bitmask
+
+generator_a_initial = int(input().split(' ')[-1])
+generator_a_multiplier = 16807
+generator_a = generator(generator_a_initial, generator_a_multiplier)
+
+generator_b_initial = int(input().split(' ')[-1])
+generator_b_multiplier = 48271
+generator_b = generator(generator_b_initial, generator_b_multiplier)
+
+matches = 0
+for _ in range(40_000_000):
+    if next(generator_a) == next(generator_b):
+        matches += 1
+
+print(matches)
