e1aa31f1e3669bafb59f20f044c0f6ea30a53381
nibley <5225323+nibley@users.noreply.github.com>
2024-09-25 15:29:48 -0600

    update shell QOL

--- meta/shellQOL.sh
+++ meta/shellQOL.sh
@@ -1,6 +1,8 @@
 # windows
-alias paste='cat /dev/clipboard >> input.txt'
 alias python="$HOME/AppData/Local/Programs/Python/Python312/python.exe"
 alias pythonrepl="winpty $HOME/AppData/Local/Programs/Python/Python312/python.exe"
 alias copy2='cat 1.py >> 2.py'
 alias template='cat ../../meta/template.py >> 1.py'
+paste() {
+    cat /dev/clipboard >> "${1:-input}.txt"
+}
