041370f617fb85c86b2f572ed7907bdec8255141
nibley <5225323+nibley@users.noreply.github.com>
2024-11-06 13:34:40 -0700

    2022 1

---
+++ 2022/1/1.py
@@ -0,0 +1,16 @@
+calorie_amounts = []
+current_calorie_amount = 0
+while True:
+    try:
+        line = input()
+    except EOFError:
+        current_calorie_amount = 0
+        break
+
+    if line:
+        current_calorie_amount += int(line)
+    else:
+        calorie_amounts.append(current_calorie_amount)
+        current_calorie_amount = 0
+
+print(sorted(calorie_amounts)[-1])
