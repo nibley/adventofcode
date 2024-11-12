a1213a07920a92c7fbc76da6cbd5a8e8da51950f
nibley <5225323+nibley@users.noreply.github.com>
2024-11-11 19:27:30 -0700

    2017 18 (part 2 which had been skipped, also rewrite part 1)

--- 2017/18/1.py
+++ 2017/18/1.py
@@ -2,51 +2,47 @@
 
 from collections import defaultdict
 
-def get_register(register_name):
-    if type(register_name) is not str:
-        return register_name # was a constant, not a register name
-
-    return registers[register_name]
-
 registers = defaultdict(lambda: 0)
+get_register = lambda value: (
+    value if type(value) is not str
+    else registers[value]
+)
+
 last_sound_frequency = None
 last_recovered_frequency = None
 def run_instruction(i):
-    if i > max_instruction_index:
-        raise ValueError
-
     operation, arguments = program[i]
-    print(i, operation, arguments)
 
-    if operation == 'snd':
-        frequency = get_register(arguments[0])
-        global last_sound_frequency
-        last_sound_frequency = frequency
-    elif operation == 'set':
-        register_name, value = arguments
-        value = get_register(value)
-        registers[register_name] = value
-    elif operation == 'add':
-        register_name, value = arguments
-        value = get_register(value)
-        registers[register_name] += value
-    elif operation == 'mul':
-        register_name, value = arguments
-        value = get_register(value)
-        registers[register_name] *= value
-    elif operation == 'mod':
-        register_name, value = arguments
-        value = get_register(value)
-        registers[register_name] %= value
-    elif operation == 'rcv':
-        test_value = get_register(arguments[0])
-        if test_value != 0:
+    if operation in ('snd', 'rcv'):
+        argument, *_ = arguments
+        argument = get_register(argument)
+
+        if operation == 'snd':
+            global last_sound_frequency
+            last_sound_frequency = argument
+        elif operation == 'rcv' and argument:
+            assert last_sound_frequency is not None
             global last_recovered_frequency
             last_recovered_frequency = last_sound_frequency
     elif operation == 'jgz':
         test_value, offset = map(get_register, arguments)
         if test_value > 0:
             return i + offset
+    else:
+        register_name, argument = arguments
+        value = registers[register_name]
+        argument = get_register(argument)
+
+        if operation == 'set':
+            value = argument
+        elif operation == 'add':
+            value += argument
+        elif operation == 'mul':
+            value *= argument
+        elif operation == 'mod':
+            value = value % argument
+
+        registers[register_name] = value
 
     return i + 1
 
@@ -58,22 +54,19 @@ while True:
         break
 
     operation, *arguments = line.split(' ')
-    arguments = [ argument if argument.isalpha() else int(argument) \
-        for argument in arguments ]
+    arguments = [
+        argument if argument.isalpha() else int(argument)
+        for argument in arguments
+    ]
     program.append( (operation, arguments) )
 
-max_instruction_index = len(program) - 1
+max_instruction_index = len(program)
+
 next_instruction = 0
-try:
-    while last_recovered_frequency is None:
-        next_instruction = run_instruction(next_instruction)
-except ValueError:
-    pass
-finally:
-    print()
-    print('FINAL STATE')
-    for key in sorted(registers.keys()):
-        print(f'{key}: \t{registers[key]}')
-    print()
-    print('Last recovered frequency:')
-    print(last_recovered_frequency)
+while (
+    last_recovered_frequency is None
+    and 0 <= next_instruction < max_instruction_index
+):
+    next_instruction = run_instruction(next_instruction)
+
+print(last_recovered_frequency)
--- 2017/18/2.py
+++ 2017/18/2.py
@@ -1,67 +1,56 @@
 # modified from 2015 23
 
-from collections import defaultdict
+from collections import defaultdict, deque
 
-message_queues = {}
-programs_sleeping = {}
-program_send_counts = defaultdict(lambda: 0)
-def program_instance(instance_id):
-    message_queues[instance_id] = []
-    programs_sleeping[instance_id] = False
-    registers = defaultdict(lambda: 0, [('p', instance_id)])
+def make_duet_program(program_id):
+    registers = defaultdict(lambda: 0, {'p': program_id})
+    get_register = lambda value: (
+        value if type(value) is not str
+        else registers[value]
+    )
+    message_queue = message_queues[program_id]
+    other_message_queue = message_queues[1 if program_id == 0 else 0]
 
-    def get_register(register_name):
-        if type(register_name) is not str:
-            return register_name # was a constant, not a register name
-
-        return registers[register_name]
-
-    def run_instruction(i):
-        if i > max_instruction_index:
-            raise ValueError
-
+    def duet_program(i):
         operation, arguments = program[i]
-        # if instance_id == 1:
-        # if instance_id == 0:
-        if False:
-            print(i, operation, arguments)
+        blocked[program_id] = False
 
         if operation == 'snd':
-            message = get_register(arguments[0])
-            message_queues[0 if instance_id == 1 else 1].append(message)
-            program_send_counts[instance_id] += 1
-        elif operation == 'rcv':
-            if not message_queues[instance_id]:
-                programs_sleeping[instance_id] = True
-                return i
+            argument, *_ = arguments
+            argument = get_register(argument)
 
-            programs_sleeping[instance_id] = False
+            other_message_queue.append(argument)
+            send_counts[program_id] += 1
+        elif operation == 'rcv':
+            if not message_queue:
+                blocked[program_id] = True
+                return i # wait
+
             register_name, *_ = arguments
-            registers[register_name] = message_queues[instance_id].pop()
-        elif operation == 'set':
-            register_name, value = arguments
-            value = get_register(value)
-            registers[register_name] = value
-        elif operation == 'add':
-            register_name, value = arguments
-            value = get_register(value)
-            registers[register_name] += value
-        elif operation == 'mul':
-            register_name, value = arguments
-            value = get_register(value)
-            registers[register_name] *= value
-        elif operation == 'mod':
-            register_name, value = arguments
-            value = get_register(value)
-            registers[register_name] %= value
+            registers[register_name] = message_queue.popleft()
         elif operation == 'jgz':
             test_value, offset = map(get_register, arguments)
             if test_value > 0:
                 return i + offset
+        else:
+            register_name, argument = arguments
+            value = registers[register_name]
+            argument = get_register(argument)
+
+            if operation == 'set':
+                value = argument
+            elif operation == 'add':
+                value += argument
+            elif operation == 'mul':
+                value *= argument
+            elif operation == 'mod':
+                value = value % argument
+
+            registers[register_name] = value
 
         return i + 1
-    
-    return run_instruction
+
+    return duet_program
 
 program = []
 while True:
@@ -71,34 +60,31 @@ while True:
         break
 
     operation, *arguments = line.split(' ')
-    arguments = [ argument if argument.isalpha() else int(argument) \
-        for argument in arguments ]
+    arguments = [
+        argument if argument.isalpha() else int(argument)
+        for argument in arguments
+    ]
     program.append( (operation, arguments) )
 
-max_instruction_index = len(program) - 1
-program_0 = program_instance(0)
-program_0_next_instruction = 0
-program_1 = program_instance(1)
-program_1_next_instruction = 0
-try:
-    loops = 0
-    while not all(programs_sleeping.values()):
-        program_0_next_instruction = program_0(program_0_next_instruction)
-        program_1_next_instruction = program_1(program_1_next_instruction)
+max_instruction_index = len(program)
 
-        if not loops % 500000:
-            print(f'{program_send_counts[0]}\t{program_send_counts[1]}')
-        # print(f'{program_0_next_instruction}\t{program_1_next_instruction}')
-        # print(message_queues.items())
-        loops += 1
-    print(':( deadlock')
-except ValueError:
-    # pass
-    print('error')
-finally:
-    print()
-    print(program_send_counts.items())
-    print(message_queues.items())
-    print()
-    print('Program 1 sent messages:')
-    print(program_send_counts[1])
+message_queues = (deque(), deque())
+blocked = [False, False]
+send_counts = [0, 0]
+next_instructions = [0, 0]
+duet_programs = tuple(
+    make_duet_program(program_id) for program_id in range(2)
+)
+while (
+    not all(blocked)
+    and all(
+        0 <= next_instruction < max_instruction_index
+        for next_instruction in next_instructions
+    )
+):
+    for program_id, (next_instruction, duet_program) in enumerate(
+        zip(next_instructions, duet_programs)
+    ):
+        next_instructions[program_id] = duet_program(next_instruction)
+
+print(send_counts[1])
