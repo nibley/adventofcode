1626a90902eebb0bdd0d8de3b12a3f767dc69f84
nibley <5225323+nibley@users.noreply.github.com>
2024-11-28 16:53:11 -0700

    rewrite 2015 13

--- 2015/13/1.py
+++ 2015/13/1.py
@@ -1,48 +1,37 @@
 from itertools import permutations
-from math import inf
-
-feelings = {}
 
 def circular_permutations(people):
     # from https://stackoverflow.com/a/51532160
-    gen = permutations(people[1:])
-    return [[people[0]] + list(end) for end in gen]
+    head, *tail = people
+    for tail_permutation in permutations(tail):
+        yield (head, *tail_permutation)
 
 def happiness_score(people):
-    pairs = []
-    score = 0
-    for i, person in enumerate(people[:-1]):
-        pairs.append((person, people[i + 1]))
-    pairs.append((people[-1], people[0]))
+    return sum(
+        (
+            feelings[first_person][second_person]
+            + feelings[second_person][first_person]
+        )
+        for first_person, second_person in zip(
+            people,
+            (*people[ 1 : ], people[0])
+        )
+    )
 
-    for first_person, second_person in pairs:
-        score += feelings[first_person][second_person]
-        score += feelings[second_person][first_person]
-    
-    return score
-
+feelings = {}
 while True:
     try:
         line = input()
     except EOFError:
         break
-    
-    left_side, person_object = line.split(' happiness units by sitting next to ')
-    person_subject, _, valence, n = left_side.split(' ')
-    n = int(n)
-    if valence == 'lose':
-        n = -1 * n
+
+    *rest, person_object = line.split()
+    person_object = person_object[ : -1 ]
+    person_subject, _, valence, units, *_ = rest
+    delta = int(units) * (-1 if valence == 'lose' else 1)
+
     feelings.setdefault(person_subject, {})
-    feelings[person_subject][person_object[:-1]] = n
+    feelings[person_subject][person_object] = delta
 
-arrangements = circular_permutations(list(feelings.keys()))
-highest_score = -inf
-highest_score_arrangement = None
-for arrangement in arrangements:
-    score = happiness_score(arrangement)
-    if score > highest_score:
-        highest_score = score
-        highest_score_arrangement = arrangement
-
-print(' '.join(highest_score_arrangement))
-print(highest_score)
+arrangements = circular_permutations(feelings)
+print(max(map(happiness_score, arrangements)))
--- 2015/13/2.py
+++ 2015/13/2.py
@@ -1,54 +1,41 @@
 from itertools import permutations
-from math import inf
-
-feelings = {}
-
-def get_feeling(first_person, second_person):
-    if first_person is None or second_person is None:
-        return 0
-    else:
-        return feelings[first_person][second_person]
 
 def circular_permutations(people):
     # from https://stackoverflow.com/a/51532160
-    gen = permutations(people[1:])
-    return [[people[0]] + list(end) for end in gen]
+    head, *tail = people
+    for tail_permutation in permutations(tail):
+        yield (head, *tail_permutation)
 
 def happiness_score(people):
-    pairs = []
-    score = 0
-    for i, person in enumerate(people[:-1]):
-        pairs.append((person, people[i + 1]))
-    pairs.append((people[-1], people[0]))
+    return sum(
+        (
+            feelings[first_person][second_person]
+            + feelings[second_person][first_person]
+        )
+        for first_person, second_person in zip(
+            people,
+            (*people[ 1 : ], people[0])
+        )
+    )
 
-    for first_person, second_person in pairs:
-        score += get_feeling(first_person, second_person)
-        score += get_feeling(second_person, first_person)
-    
-    return score
-
+# '' will represent me
+feelings = {}
 while True:
     try:
         line = input()
     except EOFError:
         break
-    
-    left_side, person_object = line.split(' happiness units by sitting next to ')
-    person_subject, _, valence, n = left_side.split(' ')
-    n = int(n)
-    if valence == 'lose':
-        n = -1 * n
+
+    *rest, person_object = line.split()
+    person_object = person_object[ : -1 ]
+    person_subject, _, valence, units, *_ = rest
+    delta = int(units) * (-1 if valence == 'lose' else 1)
+
     feelings.setdefault(person_subject, {})
-    feelings[person_subject][person_object[:-1]] = n
+    feelings[person_subject][person_object] = delta
+    feelings[person_subject][''] = 0
 
-arrangements = circular_permutations(list(feelings.keys()) + [None])
-highest_score = -inf
-highest_score_arrangement = None
-for arrangement in arrangements:
-    score = happiness_score(arrangement)
-    if score > highest_score:
-        highest_score = score
-        highest_score_arrangement = arrangement
+feelings[''] = dict.fromkeys(feelings, 0)
 
-print(' '.join(str(person) for person in highest_score_arrangement))
-print(highest_score)
+arrangements = circular_permutations(feelings)
+print(max(map(happiness_score, arrangements)))
