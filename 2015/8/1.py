d2fb3474c9225ef9bd0f38dd09f5cd7064c4257d
nibley <5225323+nibley@users.noreply.github.com>
2024-11-27 22:53:22 -0700

    rewrite 2015 8

--- 2015/8/1.py
+++ 2015/8/1.py
@@ -1,28 +1,22 @@
 from re import sub
-total_code = 0
-total_memory = 0
 
-r"""
-escape types
-
-\\
-\"
-\x{n}{n}
-"""
-
-def memory_chars(line):
-    line = line.replace(r'\\', '_')
-    line = line.replace(r'\"', '_')
-    line = sub(r'\\x[a-f0-9]{2}', '_', line)
-    return len(line) - 2
-
+lines = []
 while True:
     try:
       line = input()
     except EOFError:
         break
 
-    total_code += len(line)
-    total_memory += memory_chars(line)
+    lines.append(line)
 
-print(total_code - total_memory)
+len_in_memory = lambda line: len(
+    sub(
+        r'\\x[a-f0-9]{2}',
+        '_',
+        line
+            .replace(r'\\', '_')
+            .replace(r'\"', '_')
+    )
+) - 2
+
+print(sum( len(line) - len_in_memory(line) for line in lines ))
