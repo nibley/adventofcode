a8ce24848385365f40ab3cc780dc3bd90308bf80
nibley <5225323+nibley@users.noreply.github.com>
2024-11-28 22:49:34 -0700

    rewrite 2015 16

--- 2015/16/1.py
+++ 2015/16/1.py
@@ -1,38 +1,20 @@
 clues = {}
-
-def accuse_aunt(aunt_number):
-    aunt_clues = clues[aunt_number]
-    for clue_name, clue_value in aunt_clues.items():
-        if clue_name in knowledge and knowledge[clue_name] != clue_value:
-            return False
-    
-    # didn't end up needing to try the given knowledge against the aunt
-    
-    return True
-
-def parse_aunt(line):
-    pieces = line.split(', ')
-    aunt_info, *real_first_piece = pieces[0].split(': ')
-    pieces[0] = ': '.join(real_first_piece)
-    
-    aunt_number = aunt_info.split(' ')[1]
-    aunt_clues = {}
-
-    for piece in pieces:
-        clue_name, clue_value = piece.split(': ')
-        aunt_clues[clue_name] = int(clue_value)
-
-    clues[aunt_number] = aunt_clues
-
 while True:
     try:
         line = input()
     except EOFError:
         break
-    
-    parse_aunt(line)
 
-knowledge = {
+    _, aunt_number, *aunt_clues = line.replace(':', '').split()
+    clues[ int(aunt_number) ] = {
+        clue_name : int(clue_value.replace(',', ''))
+        for clue_name, clue_value in zip(
+            aunt_clues[   : -1 : 2 ],
+            aunt_clues[ 1 :    : 2 ]
+        )
+    }
+
+KNOWLEDGE = {
     'children': 3,
     'cats': 7,
     'samoyeds': 2,
@@ -45,7 +27,13 @@ knowledge = {
     'perfumes': 1
 }
 
-for aunt_number in clues:
-    if accuse_aunt(aunt_number):
-        print(aunt_number)
-        break
+print(
+    next(
+        aunt_number
+        for aunt_number, aunt_clues in clues.items()
+        if all(
+            KNOWLEDGE.get(clue_name) == clue_value
+            for clue_name, clue_value in aunt_clues.items()
+        )
+    )
+)
