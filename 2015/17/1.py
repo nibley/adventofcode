b8e28c4660eee1c9673e0c32bf3dfca604f79c2b
nibley <5225323+nibley@users.noreply.github.com>
2024-11-29 00:01:15 -0700

    rewrite 2015 17

--- 2015/17/1.py
+++ 2015/17/1.py
@@ -4,13 +4,20 @@ containers = []
 while True:
     try:
         line = input()
-        containers.append(int(line))
     except EOFError:
         break
 
-all_combinations = list()
-for i in range(len(containers)):
-    all_combinations.extend(combinations(containers, i + 1))
+    containers.append(int(line))
 
-valid_combinations = list(combination for combination in all_combinations if sum(combination) == 150)
-print(len(valid_combinations))
+combinations_by_length = (
+    combinations(containers, i)
+    for i in range(1, len(containers) + 1)
+)
+
+print(
+    sum(
+        sum(combination) == 150
+        for combinations in combinations_by_length
+        for combination in combinations
+    )
+)
--- 2015/17/2.py
+++ 2015/17/2.py
@@ -4,19 +4,21 @@ containers = []
 while True:
     try:
         line = input()
-        containers.append(int(line))
     except EOFError:
         break
 
-all_combinations = list()
-for i in range(len(containers)):
-    all_combinations.extend(combinations(containers, i + 1))
+    containers.append(int(line))
 
-valid_combinations = list(combination for combination in all_combinations if sum(combination) == 150)
-by_number = {} # number of containers to number of different ways under that number
-for combination in valid_combinations:
-    number = len(combination)
-    by_number.setdefault(number, 0)
-    by_number[number] += 1
+combinations_by_length = (
+    combinations(containers, i)
+    for i in range(1, len(containers) + 1)
+)
 
-print(by_number[ sorted(by_number.keys())[0] ])
+print(
+    min(
+        len(combination)
+        for combinations in combinations_by_length
+        for combination in combinations
+        if sum(combination) == 150
+    )
+)
