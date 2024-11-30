0f83fc8514e906f9a9061e57571030ed64290e68
nibley <5225323+nibley@users.noreply.github.com>
2024-11-29 23:47:15 -0700

    rewrite 2016 4

--- 2016/4/1.py
+++ 2016/4/1.py
@@ -1,26 +1,30 @@
+from collections import Counter
+
 rooms = []
 while True:
     try:
         line = input()
     except EOFError:
         break
-    
-    left_side, right_side = line.split('[')
-    *name, sector_id = left_side.split('-')
-    checksum = right_side.replace(']', '')
-    rooms.append((int(sector_id), ''.join(name), checksum))
 
-total = 0
-for sector_id, name, checksum in rooms:
-    frequencies = {}
-    for char in name:
-        frequencies.setdefault(char, 0)
-        frequencies[char] += 1
-    most_common = sorted(frequencies.items(), key=lambda item: (-item[1], item[0]))[:5]
-    
-    # print(most_common)
-    
-    if checksum == ''.join([item[0] for item in most_common]):
-        total += sector_id
+    name_and_room_id, checksum = line.split('[')
+    *name_pieces, room_id = name_and_room_id.split('-')
 
-print(total)
+    rooms.append(
+        (
+            ''.join(name_pieces),
+            int(room_id),
+            checksum[ : -1 ] # exclude final ]
+        )
+    )
+
+print(
+    sum(
+        room_id
+        for name, room_id, checksum in rooms
+        if checksum == ''.join(
+            letter
+            for letter, _ in Counter(sorted(name)).most_common(5)
+        )
+    )
+)
