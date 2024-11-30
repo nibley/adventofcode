6c1f1d4167c0c1f7697637d3fb9d04e651f22553
nibley <5225323+nibley@users.noreply.github.com>
2024-11-29 19:27:06 -0700

    rewrite 2016 2

--- 2016/2/1.py
+++ 2016/2/1.py
@@ -1,58 +1,38 @@
 buttons = {
-    -1: {
-        -1: '7',
-        0: '4',
-        1: '1',
-    },
-    0: {
-        -1: '8',
-        0: '5',
-        1: '2',
-    },
-    1: {
-        -1: '9',
-        0: '6',
-        1: '3',
-    },
+    (-1, -1) : '1', (-1,  0) : '4', (-1, 1) : '7',
+    ( 0, -1) : '2', ( 0,  0) : '5', ( 0, 1) : '8',
+    ( 1, -1) : '3', ( 1,  0) : '6', ( 1, 1) : '9'
 }
 
-def make_move(direction):
-    if direction == 'U':
-        blocked = y == 1
-    elif direction == 'D':
-        blocked = y == -1
-    elif direction == 'L':
-        blocked = x == -1
-    elif direction == 'R':
-        blocked = x == 1
-    
-    if blocked:
-        return (x, y)
-    
-    if direction == 'U':
-        return (x, y + 1)
-    elif direction == 'D':
-        return (x, y - 1)
-    elif direction == 'L':
-        return (x - 1, y)
-    elif direction == 'R':
-        return (x + 1, y)
+def make_move(position, direction):
+    x, y = position
 
-lines = []
+    if direction == 'U':
+        new_position = (x, y - 1)
+    elif direction == 'D':
+        new_position = (x, y + 1)
+    elif direction == 'L':
+        new_position = (x - 1, y)
+    elif direction == 'R':
+        new_position = (x + 1, y)
+
+    return new_position if new_position in buttons else position
+
+moves = []
 while True:
     try:
         line = input()
     except EOFError:
         break
-    
-    lines.append(line)
 
-x, y = (0, 0)
+    moves.append(line)
+
+position = (0, 0)
 code = ''
-for line in lines:
-    for direction in line:
-        x, y = make_move(direction)
-    button = buttons[x][y]
-    code += button
+for move in moves:
+    for direction in move:
+        position = make_move(position, direction)
+
+    code += buttons[position]
 
 print(code)
--- 2016/2/2.py
+++ 2016/2/2.py
@@ -1,69 +1,40 @@
 buttons = {
-    -2: {
-        0: '5',
-    },
-    -1: {
-        -1: 'A',
-        0: '6',
-        1: '2',
-    },
-    0: {
-        -2: 'D',
-        -1: 'B',
-        0: '7',
-        1: '3',
-        2: '1',
-    },
-    1: {
-        -1: 'C',
-        0: '8',
-        1: '4',
-    },
-    2: {
-        0: '9',
-    },
+                                   (0, -2) : '1',
+                   (-1, -1) : '2', (0, -1) : '3', (1, -1) : '4',
+    (-2, 0) : '5', (-1,  0) : '6', (0,  0) : '7', (1,  0) : '8', (2, 0) : '9',
+                   (-1,  1) : 'A', (0,  1) : 'B', (1,  1) : 'C',
+                                   (0,  2) : 'D'
 }
 
-def make_move(direction):
-    current_button = buttons[x][y]
+def make_move(position, direction):
+    x, y = position
 
     if direction == 'U':
-        blocked = current_button in '52149'
+        new_position = (x, y - 1)
     elif direction == 'D':
-        blocked = current_button in '5ADC9'
+        new_position = (x, y + 1)
     elif direction == 'L':
-        blocked = current_button in '125AD'
+        new_position = (x - 1, y)
     elif direction == 'R':
-        blocked = current_button in '149CD'
-    
-    if blocked:
-        return (x, y)
-    
-    if direction == 'U':
-        return (x, y + 1)
-    elif direction == 'D':
-        return (x, y - 1)
-    elif direction == 'L':
-        return (x - 1, y)
-    elif direction == 'R':
-        return (x + 1, y)
+        new_position = (x + 1, y)
 
-lines = []
+    return new_position if new_position in buttons else position
+
+moves = []
 while True:
     try:
         line = input()
     except EOFError:
         break
-    
-    lines.append(line)
 
-x, y = (0, 0)
+    moves.append(line)
+
+position = (0, 0)
 code = ''
-for line in lines:
-    for direction in line:
-        # print(f'{buttons[x][y]} {direction}')
-        x, y = make_move(direction)
-    button = buttons[x][y]
-    code += button
+for move in moves:
+    for direction in move:
+        position = make_move(position, direction)
+
+    code += buttons[position]
 
 print(code)
