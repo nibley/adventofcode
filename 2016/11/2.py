c6f11ff07232c30f662ca7f1f6bdcc6def9b2f0e
nibley <5225323+nibley@users.noreply.github.com>
2024-12-02 16:26:23 -0700

    rewrite 2016 11

--- 2016/11/2.py
+++ 2016/11/2.py
@@ -1,6 +1,46 @@
 from itertools import combinations, chain, product as cartesian_product
 from functools import cache
-from collections import deque
+
+starting_items = []
+while True:
+    try:
+        line = input()
+    except EOFError:
+        break
+
+    _, line = line.split(' contains ')
+    items_raw = (
+        line
+            .replace(', and ', ', ')
+            .replace(' and ', ', ')
+            .replace('.', '')
+            .split(', ')
+    )
+
+    if items_raw[0] == 'nothing relevant':
+        starting_items.append(frozenset())
+        continue
+
+    items = []
+    for item in items_raw:
+        _, item_material, item_type = item.split()
+
+        item_type = 'm' if item_type == 'microchip' else 'g'
+        if item_type == 'm':
+            item_material, _ = item_material.split('-')
+
+        items.append( (item_type, item_material) )
+
+    starting_items.append(frozenset(items))
+
+starting_items[0] = starting_items[0].union(
+    {
+        ('m', 'elerium'),
+        ('g', 'elerium'),
+        ('m', 'dilithium'),
+        ('g', 'dilithium')
+    }
+)
 
 @cache
 def chips_would_be_safe(floor_items):
@@ -39,10 +79,12 @@ def get_successor_states(state):
 
     move_options = cartesian_product(floor_options, carried_items_options)
     for next_floor_number, carried_items in move_options:
-        next_floor_items = \
-            get_floor_from_floors(next_floor_number, floors).union(carried_items)
-        left_behind_items = \
-            current_floor_items.difference(carried_items)
+        next_floor_items = get_floor_from_floors(
+            next_floor_number,
+            floors
+        ).union(carried_items)
+
+        left_behind_items = current_floor_items.difference(carried_items)
 
         if (
             chips_would_be_safe(next_floor_items)
@@ -50,53 +92,18 @@ def get_successor_states(state):
         ):
             yield ( # a state object as described below
                 next_floor_number,
-                frozenset((
-                    (current_floor, left_behind_items),
-                    (next_floor_number, next_floor_items),
-                    *(
-                        (i, floor)
-                        for i, floor in floors
-                        if i not in (current_floor, next_floor_number)
+                frozenset(
+                    (
+                        (current_floor, left_behind_items),
+                        (next_floor_number, next_floor_items),
+                        *(
+                            (i, floor)
+                            for i, floor in floors
+                            if i not in {current_floor, next_floor_number}
+                        )
                     )
-                ))
+                )
             )
-
-starting_floors = []
-while True:
-    try:
-        line = input()
-    except EOFError:
-        break
-
-    left_side, right_side = line[:-1].split(' contains ')
-    if right_side.startswith('nothing'):
-        starting_floors.append(frozenset())
-        continue
-
-    items = right_side.split(', ')
-    if len(items) == 1: # two items, so no commas
-        items = items[0].split(' and ')
-
-    floor_items = []
-    for item in items:
-        pieces = item.split(' ')
-        item_material, item_type = pieces[-2:]
-
-        item_type = 'm' if item_type == 'microchip' else 'g'
-
-        if item_type == 'm':
-            item_material = item_material.replace('-compatible', '')
-
-        floor_items.append( (item_type, item_material) )
-
-    starting_floors.append(frozenset(floor_items))
-
-starting_floors[0] = starting_floors[0].union([
-    ('m', 'elerium'),
-    ('g', 'elerium'),
-    ('m', 'dilithium'),
-    ('g', 'dilithium')
-])
 
 # a state object is (n, floors)
 # where n is the current floor (of the player character and elevator)
@@ -107,19 +114,25 @@ starting_floors[0] = starting_floors[0].union([
 #
 # this all lets us do the entire problem with no mutations
 # to these state objects, as well as making them hashable!
-starting_state = (0, frozenset(enumerate(starting_floors)))
+starting_state = (0, frozenset(enumerate(starting_items)))
 
 done = False
 known_state_hashes = set([hash(starting_state)])
-states_to_crawl = deque([starting_state])
+states_to_crawl = [starting_state]
 steps = 0
-while states_to_crawl:
-    new_states_found = deque()
+while not done and states_to_crawl:
+    new_states_found = []
     steps += 1
+
+    print(steps)
 
     for state in states_to_crawl:
         for successor_state in get_successor_states(state):
-            if not any(floor for i, floor in successor_state[1] if i != 3):
+            if not any(
+                floor
+                for i, floor in successor_state[1]
+                if i != 3
+            ):
                 done = True
                 break
 
@@ -130,9 +143,6 @@ while states_to_crawl:
 
         if done:
             break
-
-    if done:
-        break
 
     states_to_crawl = new_states_found
 
