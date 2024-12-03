718101f5e87808521ed7078f3d1a2ecff4e7b5c0
nibley <5225323+nibley@users.noreply.github.com>
2024-12-03 03:40:17 -0700

    rewrite 2016 18

--- 2016/18/2.py
+++ 2016/18/2.py
@@ -1,28 +1,26 @@
-trap_cases = [
+TRAP_CASES = (
     (False, False, True),
-    (True, False, False),
-    (False, True, True),
-    (True, True, False),
-]
-def generate_cell(i, prev_row):
-    left = right = True
-    center = prev_row[i]
-    if i != 0:
-        left = prev_row[i - 1]
-    if i != width - 1:
-        right = prev_row[i + 1]
-    
-    return not ( (left, center, right) in trap_cases )
+    (True,  False, False),
+    (False, True,  True),
+    (True,  True,  False)
+)
+def generate_cell(x, last_row):
+    center = last_row[x]
+    left = True if x == 0 else last_row[x - 1]
+    right = True if x == WIDTH - 1 else last_row[x + 1]
 
-prev_row = [char == '.' for char in input()]
-total_safe = len([tile for tile in prev_row if tile])
-width = len(prev_row)
-num_rows = 400000
-for num_row in range(1, num_rows):
-    new_row = [generate_cell(i, prev_row) for i in range(width)]
-    for tile in new_row:
-        if tile:
-            total_safe += 1
-    prev_row = new_row
+    return (left, center, right) not in TRAP_CASES
 
-print(total_safe)
+# True for safe, False for trap
+FIRST_ROW = tuple( char == '.' for char in input() )
+WIDTH = len(FIRST_ROW)
+
+def simulate(first_row):
+    yield sum(first_row)
+
+    last_row = first_row
+    for _ in range(1, 400_000):
+        last_row = tuple( generate_cell(x, last_row) for x in range(WIDTH) )
+        yield sum(last_row)
+
+print(sum(simulate(FIRST_ROW)))
