380878155a163d73b9ed0f9714ff2056baaa22f3
nibley <5225323+nibley@users.noreply.github.com>
2024-12-04 22:34:08 -0700

    clean up 2024 5 a little bit

--- 2024/5/1.py
+++ 2024/5/1.py
@@ -1,9 +1,8 @@
 rules = []
 line = input()
 while line:
-    a, b = map(int, line.split('|'))
+    rules.append(tuple(map(int, line.split('|'))))
     line = input()
-    rules.append((a, b))
 
 updates = []
 while True:
@@ -12,39 +11,29 @@ while True:
     except EOFError:
         break
 
-    updates.append(
-        tuple(map(int, line.split(',')))
-    )
-
-for update in updates:
+    update = tuple(map(int, line.split(',')))
     assert len(update) % 2
+
+    updates.append(update)
 
 def valid(update):
     for first, second in zip(
-        update[:-1], update[1:]
+        update[    : -1 ],
+        update[ 1  :    ]
     ):
         for before, after in rules:
-            if set((before, after)) == set((first, second)):
-                print(1)
-                if not (before == first and after == second):
-                    print('bad', first, second, ' | ', before, after)
-                    return False
+            if (
+                set( (before, after) ) == set( (first, second) )
+                and not (before == first and after == second)
+            ):
+                return False
 
     return True
 
-valid_updates = tuple(
-    update
-    for update in updates
-    if valid(update)
+print(
+    sum(
+        update[ len(update) // 2 ]
+        for update in updates
+        if valid(update)
+    )
 )
-
-for update in valid_updates:
-    print(update)
-
-total = 0
-for update in valid_updates:
-    i = len(update) // 2
-    total += update[i]
-    # print(update[i])
-
-print(total)
--- 2024/5/2.py
+++ 2024/5/2.py
@@ -3,9 +3,8 @@ from itertools import permutations
 rules = []
 line = input()
 while line:
-    a, b = map(int, line.split('|'))
+    rules.append(tuple(map(int, line.split('|'))))
     line = input()
-    rules.append((a, b))
 
 updates = []
 while True:
@@ -14,84 +13,58 @@ while True:
     except EOFError:
         break
 
-    updates.append(
-        list(map(int, line.split(',')))
-    )
-
-for update in updates:
+    update = list(map(int, line.split(',')))
     assert len(update) % 2
+
+    updates.append(update)
 
 def valid(update):
     for first, second in zip(
-        update[:-1], update[1:]
+        update[    : -1 ],
+        update[ 1  :    ]
     ):
         for before, after in rules:
-            if set((before, after)) == set((first, second)):
-                if not (before == first and after == second):
-                    return False
+            if (
+                set( (before, after) ) == set( (first, second) )
+                and not (before == first and after == second)
+            ):
+                return False
 
     return True
 
-# def fix(update):
-#     print('fix')
-#     for permutation in permutations(update):
-#         permutation = tuple(permutation)
-#         if valid(permutation):
-#             return permutation
-
-#     assert False
-
 def fix(update):
-    update = update[:]
-    # print('fix', update)
+    print('fix')
 
-    result = list(update)
-
-    for i, (first, second) in enumerate(zip(
-        update[:-1], update[1:]
-    )):
+    for (i, first), (j, second) in zip(
+        enumerate(update[    : -1 ]),
+        enumerate(update[ 1  :    ], start=1)
+    ):
         for before, after in rules:
-            if set((before, after)) == set((first, second)):
-                if (before == first and after == second):
-                    # result.append(first)
-                    # result.append(second)
-                    pass
-                else:
-                    # print(update, update[i], update[i + 1], i, len(update))
-                    result[i], result[i + 1] = result[i + 1], result[i]
-                    # print(result)
-                    # break
-                    return result
-
-    # print()
-    return tuple(result)
+            if (
+                set( (before, after) ) == set( (first, second) )
+                and not (before == first and after == second)
+            ):
+                update[i], update[j] = update[j], update[i]
 
 invalid_updates = tuple(
-    # fix(update)
     update
     for update in updates
     if not valid(update)
 )
 
-# invalid_updates = ((75,97,47,61,53),)
-
-fixed = []
+fixed_updates = []
 for update in invalid_updates:
     while not valid(update):
-        update = fix(update)
+        # update = fix(update)
+        fix(update)
 
-    fixed.append(update)
-    print('got')
-    # print(update)
+    fixed_updates.append(update)
+    print(len(fixed_updates))
+    print()
 
-print('\n\n\n')
-
-total = 0
-for update in fixed:
-    print(update)
-
-    i = len(update) // 2
-    total += update[i]
-    # print(update[i])
-
-print(total)
+print(
+    sum(
+        update[ len(update) // 2 ]
+        for update in fixed_updates
+    )
+)
