67b330cccca4404330cb8c61659270d0a983d135
nibley <5225323+nibley@users.noreply.github.com>
2024-12-06 23:02:44 -0700

    clean up 2024 7

--- 2024/7/2.py
+++ 2024/7/2.py
@@ -1,72 +1,36 @@
 from operator import add, mul
-from collections import deque
-# from itertools import permutations
-from itertools import product
+from itertools import product as cartesian_product
 
-def concat(a, b):
-    return int(str(a) + str(b))
-
-lines = []
+equations = []
 while True:
     try:
         line = input()
     except EOFError:
         break
 
-    first, rest = line.split(': ')
-    # print(first)
-    # print(rest)
-    # print()
-    nums = tuple(map(int, rest.split()))
-    # print(nums)
+    test_value, *numbers = map(int, line.replace(':', '').split())
+    equations.append( (test_value, numbers) )
 
-    lines.append(
-        (
-            # first, deque(nums)
-            int(first), nums
-        )
-    )
+concat = lambda first, second: int(f'{first}{second}')
 
-def valid(line):
-    first, nums_orig = line
-    # for first, nums_orig in lines:
-    # print('line', first, nums_orig, len(nums_orig))
-    for ops in product((add, mul, concat), repeat=(len(nums_orig) - 1)):
-        # print(ops)
+def equation_is_possible(test_value, NUMBERS_INITIAL):
+    for operators in cartesian_product(
+        (add, mul, concat),
+        repeat=len(NUMBERS_INITIAL) - 1
+    ):
+        left_operand, *numbers = NUMBERS_INITIAL
+        for right_operand, operator in zip(numbers, operators):
+            left_operand = operator(left_operand, right_operand)
 
-        nums = deque(nums_orig)
-        # print('  have', first, nums, 'ops', ops)
-        # print('  have', first, nums)
-        left = nums.popleft()
-        # print(left)
-        # op, *ops = ops
-        while nums:
-            right = nums.popleft()
-            op, *ops = ops
-            left = op(left, right)
-            # left = nums.popleft()
-
-        # print('    end', left, type(left), type(first))
-        if left == first:
-            # print('      yes')
+        if left_operand == test_value:
             return True
-        else:
-            # print('      bad', left, ' '.join(
-                # '+' if op == add else '*'
-                # for op in ops
-            # ))
-            pass
 
-    # print('      no')
     return False
 
-total = 0
-for i, (first, nums) in enumerate(lines):
-    print(i)
-# for first, nums in [lines[0]]:
-# for first, nums in [lines[1]]:
-    if valid((first, nums)):
-        # print('\n--- yes---\n')
-        total += first
-
-print(total)
+print(
+    sum(
+        test_value
+        for test_value, numbers in equations
+        if equation_is_possible(test_value, numbers)
+    )
+)
