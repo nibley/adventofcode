aae0594a0066765ee3991843448221ca9cce8dc2
nibley <5225323+nibley@users.noreply.github.com>
2024-12-07 19:29:03 -0700

    rewrite 2017 5

--- 2017/5/2.py
+++ 2017/5/2.py
@@ -1,33 +1,22 @@
-# modified from 2015 23
-
-def run_instruction(i):
-    if i > max_instruction_index:
-        raise ValueError
-    
-    offset = program[i]
-    if offset >= 3:
-        program[i] -= 1
-    else:
-        program[i] += 1
-
-    return i + offset
-
 program = []
 while True:
     try:
         line = input()
     except EOFError:
         break
-    
-    offset = int(line)
-    program.append(offset)
 
-max_instruction_index = len(program) - 1
-next_instruction = 0
-steps_taken = 0
-try:
-    while True:
-        next_instruction = run_instruction(next_instruction)
-        steps_taken += 1
-except ValueError:
-    print(steps_taken)
+    program.append(int(line))
+
+def access_program(i):
+    offset = program[i]
+    program[i] += 1 if offset < 3 else -1
+
+    return offset
+
+steps = 0
+position = 0
+while 0 <= position < len(program):
+    steps += 1
+    position += access_program(position)
+
+print(steps)
