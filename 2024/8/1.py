6c77c666df8b35da208d3db28a4e743eb226afd5
nibley <5225323+nibley@users.noreply.github.com>
2024-12-07 22:11:21 -0700

    2024 8

---
+++ 2024/8/1.py
@@ -0,0 +1,39 @@
+from itertools import combinations
+
+nodes = {}
+y = 0
+while True:
+    try:
+        line = input()
+    except EOFError:
+        break
+
+    for x, char in enumerate(line):
+        if char != '.':
+            nodes.setdefault(char, set()).add( (x, y) )
+
+    y += 1
+
+height = y
+width = len(line)
+
+for k, v in nodes.items():
+    print(k, v)
+
+anti = set()
+
+for char, positions in nodes.items():
+    for pair in combinations(positions, 2):
+        (first_x, first_y), (second_x, second_y) = pair
+        delta_x = second_x - first_x
+        delta_y = second_y - first_y
+
+        first_anti = (second_x + delta_x, second_y + delta_y)
+        if first_anti[0] in range(width) and first_anti[1] in range(height):
+            anti.add(first_anti)
+
+        second_anti = (first_x - delta_x, first_y - delta_y)
+        if second_anti[0] in range(width) and second_anti[1] in range(height):
+            anti.add(second_anti)
+
+print(len(anti))
