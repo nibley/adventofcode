c35d5ee505e889d2ea39604084d3734784863678
nibley <5225323+nibley@users.noreply.github.com>
2024-12-09 04:40:42 -0700

    rewrite 2017 10

--- 2017/10/1.py
+++ 2017/10/1.py
@@ -1,29 +1,32 @@
-raw = input()
-knot_lengths = map(int, raw.split(','))
+KNOT_LENGTHS = map(int, input().split(','))
+ROPE_SIZE = 256
 
-rope_size = 256
-rope = [i for i in range(rope_size)]
-
+rope = [ i for i in range(ROPE_SIZE) ]
 position = 0
 skip_size = 0
-for knot_length in knot_lengths:
-    knot_region_main = slice(position, min(rope_size, position + knot_length))
-    wrap_length = position + knot_length - rope_size
+for knot_length in KNOT_LENGTHS:
+    knot_region_main = slice(
+        position,
+        min(ROPE_SIZE, position + knot_length)
+    )
+    wrap_length = position + knot_length - ROPE_SIZE
 
     if wrap_length < 1:
         rope[knot_region_main] = reversed(rope[knot_region_main])
     else:
         knot_region_wrap = slice(0, wrap_length)
-        knot_region_reversed = ( \
-            rope[knot_region_main] + rope[knot_region_wrap] \
-        )[::-1]
+        knot_region_reversed = (
+            rope[knot_region_main] + rope[knot_region_wrap]
+        )[ : : -1 ]
 
-        rope[ : knot_region_wrap.stop] = \
-            knot_region_reversed[-1 * knot_region_wrap.stop : ]
-        rope[position : ] = \
-            knot_region_reversed[ : -1 * knot_region_wrap.stop]
+        rope[ : knot_region_wrap.stop ] = (
+            knot_region_reversed[ -1 * knot_region_wrap.stop : ]
+        )
+        rope[position : ] = (
+            knot_region_reversed[ : -1 * knot_region_wrap.stop ]
+        )
 
-    position = (position + knot_length + skip_size) % rope_size
+    position = (position + knot_length + skip_size) % ROPE_SIZE
     skip_size += 1
 
 print(rope[0] * rope[1])
--- 2017/10/2.py
+++ 2017/10/2.py
@@ -1,34 +1,42 @@
 from functools import reduce
+from operator import xor
 
-raw = input()
-knot_lengths = tuple(map(ord, raw)) + (17, 31, 73, 47, 23)
+KNOT_LENGTHS = tuple(map(ord, input())) + (17, 31, 73, 47, 23)
+ROPE_SIZE = 256
 
-rope_size = 256
-rope = [i for i in range(rope_size)]
-
+rope = [ i for i in range(ROPE_SIZE) ]
 position = 0
 skip_size = 0
 for _ in range(64):
-    for knot_length in knot_lengths:
-        knot_region_main = slice(position, min(rope_size, position + knot_length))
-        wrap_length = position + knot_length - rope_size
+    for knot_length in KNOT_LENGTHS:
+        knot_region_main = slice(
+            position, min(ROPE_SIZE, position + knot_length)
+        )
+        wrap_length = position + knot_length - ROPE_SIZE
 
         if wrap_length < 1:
             rope[knot_region_main] = reversed(rope[knot_region_main])
         else:
             knot_region_wrap = slice(0, wrap_length)
-            knot_region_reversed = ( \
-                rope[knot_region_main] + rope[knot_region_wrap] \
-            )[::-1]
+            knot_region_reversed = (
+                rope[knot_region_main] + rope[knot_region_wrap]
+            )[ : : -1 ]
 
-            rope[ : knot_region_wrap.stop] = \
-                knot_region_reversed[-1 * knot_region_wrap.stop : ]
-            rope[position : ] = \
-                knot_region_reversed[ : -1 * knot_region_wrap.stop]
+            rope[ : knot_region_wrap.stop ] = (
+                knot_region_reversed[ -1 * knot_region_wrap.stop : ]
+            )
+            rope[position : ] = (
+                knot_region_reversed[ : -1 * knot_region_wrap.stop ]
+            )
 
-        position = (position + knot_length + skip_size) % rope_size
+        position = (position + knot_length + skip_size) % ROPE_SIZE
         skip_size += 1
 
-chunks = [ rope[16 * i : 16 * (i + 1)] for i in range(16) ]
-hashes = [ reduce(lambda a, b: a ^ b, chunk) for chunk in chunks ]
-print(''.join('{:02X}'.format(the_hash) for the_hash in hashes))
+print(
+    ''.join(
+        '{:02x}'.format(
+            reduce(xor, rope[16 * i : 16 * (i + 1)])
+        )
+        for i in range(16)
+    )
+)
