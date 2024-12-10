35bc2480327da7d67d970e7f1f53b3baf36e20df
nibley <5225323+nibley@users.noreply.github.com>
2024-12-09 22:29:56 -0700

    clean up 2024 10 a fair amount

--- 2024/10/1.py
+++ 2024/10/1.py
@@ -1,7 +1,6 @@
 from math import inf
 
 starts = []
-
 grid = {}
 y = 0
 while True:
@@ -11,59 +10,42 @@ while True:
         break
 
     for x, digit in enumerate(line):
-        grid[ (x, y) ] = inf if digit == '.' else int(digit)
+        position = (x, y)
         if digit == '0':
-            starts.append(
-                (x, y)
-            )
+            starts.append(position)
+
+        grid[position] = int(digit)
 
     y += 1
-
-height = y
-width = len(line)
 
 def get_score(start):
     positions_to_crawl = set([start])
     visited = set([start])
+
     while positions_to_crawl:
         new_positions_found = set()
+
         for position in positions_to_crawl:
             x, y = position
-            height = grid[ position ]
+            height = grid[position]
+
             for x_offset, y_offset in (
-                (-1, 0), (1, 0),
-                (0, -1), (0, 1)
+                ( 0, -1),
+                ( 1,  0),
+                (-1,  0),
+                ( 0,  1)
             ):
-                neighbor_x = x + x_offset
-                neighbor_y = y + y_offset
-                neighbor_height = grid.get((neighbor_x, neighbor_y), inf)
+                neighbor = (x + x_offset, y + y_offset)
+                neighbor_height = grid.get(neighbor, inf)
 
-                # visited.add(
-                    # (neighbor_x, neighbor_y)
-                # )
-
-                if neighbor_height - height != 1:
-                    continue
-
-                new_positions_found.add(
-                    (neighbor_x, neighbor_y)
-                )
+                if neighbor_height - height == 1:
+                    new_positions_found.add(neighbor)
 
         new_positions_found.difference_update(visited)
         visited.update(new_positions_found)
+
         positions_to_crawl = new_positions_found
 
-    # return 1
-    # return visited
     return sum( grid.get(position) == 9 for position in visited )
 
-print(
-    sum(
-        get_score(start)
-        # 1
-        for start in starts
-    )
-)
-
-# print(starts)
-# for x in get_score((0, 0)): print(x)
+print(sum(map(get_score, starts)))
--- 2024/10/2.py
+++ 2024/10/2.py
@@ -1,7 +1,6 @@
 from math import inf
 
 starts = []
-
 grid = {}
 y = 0
 while True:
@@ -11,71 +10,36 @@ while True:
         break
 
     for x, digit in enumerate(line):
-        grid[ (x, y) ] = inf if digit == '.' else int(digit)
+        position = (x, y)
         if digit == '0':
-            starts.append(
-                (x, y)
-            )
+            starts.append(position)
+
+        grid[position] = int(digit)
 
     y += 1
 
-height = y
-width = len(line)
-
 def get_score(start):
-    # positions_to_crawl = set([start])
     visited = set([start])
     while True:
-    # while positions_to_crawl:
-        # new_positions_found = set()
-        # for position in positions_to_crawl:
-        for position in (start, ):
-            x, y = position
-            height = grid[ position ]
+        x, y = start
+        height = grid[start]
 
-            if height == 9:
-                return 1
+        if height == 9:
+            return 1
 
-            subtotal = 0
-            for x_offset, y_offset in (
-                (-1, 0), (1, 0),
-                (0, -1), (0, 1)
-            ):
-                neighbor_x = x + x_offset
-                neighbor_y = y + y_offset
-                neighbor_height = grid.get((neighbor_x, neighbor_y), inf)
+        subtotal = 0
+        for x_offset, y_offset in (
+            ( 0, -1),
+            ( 1,  0),
+            (-1,  0),
+            ( 0,  1)
+        ):
+            neighbor = (x + x_offset, y + y_offset)
+            neighbor_height = grid.get(neighbor, inf)
 
-                # visited.add(
-                    # (neighbor_x, neighbor_y)
-                # )
+            if neighbor_height - height == 1:
+                subtotal += get_score(neighbor)
 
-                if neighbor_height - height != 1:
-                    continue
+        return subtotal
 
-                subtotal += get_score(
-                    (neighbor_x, neighbor_y)
-                )
-                # new_positions_found.add(
-                #     (neighbor_x, neighbor_y)
-                # )
-
-            return subtotal
-
-        # new_positions_found.difference_update(visited)
-        # visited.update(new_positions_found)
-        # positions_to_crawl = new_positions_found
-
-    # return 1
-    # return visited
-    return sum( grid.get(position) == 9 for position in visited )
-
-print(
-    sum(
-        get_score(start)
-        # 1
-        for start in starts
-    )
-)
-
-# print(starts)
-# for x in get_score((0, 0)): print(x)
+print(sum(map(get_score, starts)))
