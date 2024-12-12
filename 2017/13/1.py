b9c48b427ee2fdbf356863cf2c31e7673915d011
nibley <5225323+nibley@users.noreply.github.com>
2024-12-12 02:40:38 -0700

    rewrite 2017 13

--- 2017/13/1.py
+++ 2017/13/1.py
@@ -1,5 +1,25 @@
 from collections import defaultdict
 
+def scanner_position(t):
+    layer_range = layers[t]
+    cycle_length = max(1, layer_range * 2 - 2)
+
+    position = t % cycle_length
+    if position > layer_range - 1:
+        position = layer_range - (position % (layer_range - 1)) - 1
+
+    return position
+
+layers = defaultdict(lambda: 0)
+while True:
+    try:
+        line = input()
+    except EOFError:
+        break
+
+    layer_depth, layer_range = map(int, line.split(': '))
+    layers[layer_depth] = layer_range
+
 def visualize(t):
     caught = False
     print(f' t = {t}')
@@ -27,37 +47,16 @@ def visualize(t):
                     cell = '   '
             print(f'{cell} ', end='')
         print()
-    
+
     if caught:
         print()
         print(f'caught at depth {t}')
     print()
 
-def scanner_position(t, layer_range):
-    cycle_length = max(1, (layer_range * 2) - 2)
-    position = t % cycle_length
-    if position > layer_range - 1:
-        position = layer_range - (position % (layer_range - 1)) - 1
-    return position
-
-layers = defaultdict(lambda: 0)
-while True:
-    try:
-        line = input()
-    except EOFError:
-        break
-
-    layer_depth, layer_range = map(int, line.split(': '))
-    layers[layer_depth] = layer_range
-
-deepest_depth = sorted(layers.keys())[-1]
-severity = 0
-for t in range(deepest_depth + 1):
-    # visualize(t)
-    layer_range = layers[t]
-    if scanner_position(t, layer_range) == 0:
-        severity += t * layer_range
-
-print()
-print('Severity:')
-print(severity)
+print(
+    sum(
+        t * layers[t]
+        for t in range(max(layers) + 1)
+        if scanner_position(t) == 0
+    )
+)
--- 2017/13/2.py
+++ 2017/13/2.py
@@ -1,8 +1,5 @@
 from collections import defaultdict
 
-def scanner_catches(t, layer_range):
-    return t % (layer_range * 2 - 2) == 0
-
 layers = defaultdict(lambda: 0)
 while True:
     try:
@@ -13,21 +10,16 @@ while True:
     layer_depth, layer_range = map(int, line.split(': '))
     layers[layer_depth] = layer_range
 
-deepest_depth = sorted(layers.keys())[-1]
-delay = 1
-while True:
-    caught = False
-    for t in range(delay, delay + deepest_depth + 1):
-        depth = t - delay
-        if depth in layers and scanner_catches(t, layers[depth]):
-            caught = True
-            break
-    
-    if not caught:
-        break
+def scanner_catches(t, delay):
+    depth = t - delay
+    return layers[depth] and not t % (layers[depth] * 2 - 2)
 
+deepest_depth = max(layers)
+delay = 1
+while any(
+    scanner_catches(t, delay)
+    for t in range(delay, delay + deepest_depth + 1)
+):
     delay += 1
 
-print()
-print('Appropriate delay:')
 print(delay)
