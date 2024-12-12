1c232f61e452fe8d68335d4bd698d3f234b13999
nibley <5225323+nibley@users.noreply.github.com>
2024-12-12 02:47:30 -0700

    rewrite 2017 10 again

--- 2017/10/1.py
+++ 2017/10/1.py
@@ -1,9 +1,10 @@
 KNOT_LENGTHS = map(int, input().split(','))
 ROPE_SIZE = 256
 
-rope = [ i for i in range(ROPE_SIZE) ]
+rope = list(range(ROPE_SIZE))
 position = 0
 skip_size = 0
+
 for knot_length in KNOT_LENGTHS:
     knot_region_main = slice(
         position,
@@ -19,12 +20,9 @@ for knot_length in KNOT_LENGTHS:
             rope[knot_region_main] + rope[knot_region_wrap]
         )[ : : -1 ]
 
-        rope[ : knot_region_wrap.stop ] = (
-            knot_region_reversed[ -1 * knot_region_wrap.stop : ]
-        )
-        rope[position : ] = (
-            knot_region_reversed[ : -1 * knot_region_wrap.stop ]
-        )
+        split_point = knot_region_wrap.stop
+        rope[ : split_point ] = knot_region_reversed[ -1 * split_point : ]
+        rope[ position : ] = knot_region_reversed[ : -1 * split_point ]
 
     position = (position + knot_length + skip_size) % ROPE_SIZE
     skip_size += 1
--- 2017/10/2.py
+++ 2017/10/2.py
@@ -4,9 +4,10 @@ from operator import xor
 KNOT_LENGTHS = tuple(map(ord, input())) + (17, 31, 73, 47, 23)
 ROPE_SIZE = 256
 
-rope = [ i for i in range(ROPE_SIZE) ]
+rope = list(range(ROPE_SIZE))
 position = 0
 skip_size = 0
+
 for _ in range(64):
     for knot_length in KNOT_LENGTHS:
         knot_region_main = slice(
@@ -22,21 +23,16 @@ for _ in range(64):
                 rope[knot_region_main] + rope[knot_region_wrap]
             )[ : : -1 ]
 
-            rope[ : knot_region_wrap.stop ] = (
-                knot_region_reversed[ -1 * knot_region_wrap.stop : ]
-            )
-            rope[position : ] = (
-                knot_region_reversed[ : -1 * knot_region_wrap.stop ]
-            )
+            split_point = knot_region_wrap.stop
+            rope[ : split_point ] = knot_region_reversed[ -1 * split_point : ]
+            rope[ position : ] = knot_region_reversed[ : -1 * split_point ]
 
         position = (position + knot_length + skip_size) % ROPE_SIZE
         skip_size += 1
 
 print(
     ''.join(
-        '{:02x}'.format(
-            reduce(xor, rope[16 * i : 16 * (i + 1)])
-        )
+        '{:02x}'.format(reduce(xor, rope[ 16 * i : 16 * (i + 1) ]))
         for i in range(16)
     )
 )
