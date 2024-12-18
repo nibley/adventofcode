a27c837db1c35c16b5ea2d89373d38a9c30cc017
nibley <5225323+nibley@users.noreply.github.com>
2024-12-18 00:23:53 -0700

    clean up 2024 18

--- 2024/18/1.py
+++ 2024/18/1.py
@@ -2,102 +2,57 @@ from collections import defaultdict
 from math import inf
 
 grid = defaultdict(lambda: True)
-lines = []
-processed = 0
-while True:
-    if processed >= 1024: break
-    # if processed >= 12: break
-
-    try:
-        line = input()
-    except EOFError:
-        break
-
-    x, y = map(int, line.split(','))
-    # lines.append((x, y))
-    grid[(x, y)] = False
-
-    processed += 1
-
-# for line in lines: print(line)
+while len(grid) < 1024:
+    position = tuple( int(n) for n in input().split(',') )
+    grid[position] = False
 
 HEIGHT = 71
 WIDTH = 71
-# HEIGHT = 7
-# WIDTH = 7
 
-x = 0
-y = 0
-
-goal_x = WIDTH - 1
-goal_y = HEIGHT - 1
-
-def visualize():
-    for y in range(HEIGHT):
-        for x in range(WIDTH):
-            print('.' if grid[(x, y)] else '#', end='')
-        print()
-    print()
-
-visualize()
-
-DIRECTIONS = (
-    # e s w n
-    (1, 0),
-    (0, 1),
-    (-1, 0),
-    (0, -1)
-)
-
+START = (0, 0)
+GOAL = (WIDTH - 1, HEIGHT - 1)
 
 def get_neighbors(position):
     x, y = position
 
-    for x_offset, y_offset in DIRECTIONS:
+    for x_offset, y_offset in (
+        ( 1,  0),
+        ( 0,  1),
+        (-1,  0),
+        ( 0, -1)
+    ):
         neighbor_x = x + x_offset
         neighbor_y = y + y_offset
-        neighbor = (neighbor_x, neighbor_y)
 
         if (
             neighbor_x in range(WIDTH)
             and neighbor_y in range(HEIGHT)
-            and grid[neighbor]
+            and grid[(neighbor_x, neighbor_y)]
         ):
-            yield neighbor
+            yield (neighbor_x, neighbor_y)
+
+distances_from_start = defaultdict(lambda: inf, { START : 0 })
 
 unvisited = set(
-    position
-    for position, is_open in grid.items()
-    if is_open
+    (x, y)
+    for x in range(WIDTH)
+    for y in range(HEIGHT)
+    if grid[(x, y)]
 )
-
-distances_from_start = defaultdict(lambda: inf)
-distances_from_start[ (x, y) ] = 0
-visited = set()
-
 while unvisited:
-    # if not len(unvisited) % 100: print(len(unvisited))
-
     position = min(unvisited, key=distances_from_start.__getitem__)
-
-    if position == (goal_x, goal_y):
+    if position == GOAL:
         break
 
     current_score = distances_from_start[position]
     for neighbor in get_neighbors(position):
-        if neighbor in visited:
-            continue
+        if neighbor in unvisited:
+            old_score = distances_from_start[neighbor]
+            new_score = current_score + 1
 
-        old_score = distances_from_start[neighbor]
-        new_score = current_score + 1
+            if new_score < old_score:
+                distances_from_start[neighbor] = new_score
 
-        if new_score < old_score:
-            distances_from_start[neighbor] = new_score
-
-    visited.add(position)
     unvisited.remove(position)
 
-print(visited)
-print(unvisited)
-
-print(distances_from_start[(goal_x, goal_y)])
+print(distances_from_start[GOAL])
--- 2024/18/2.py
+++ 2024/18/2.py
@@ -1,123 +1,89 @@
 from collections import defaultdict
 from math import inf
 
-lines_original = []
+t = 1
+blocks = defaultdict(lambda: inf)
 while True:
     try:
         line = input()
     except EOFError:
         break
 
-    x, y = map(int, line.split(','))
-    lines_original.append((x, y))
+    position = tuple( int(n) for n in line.split(',') )
+    blocks[position] = t
 
-# for line in lines_original: print(line)
+    t += 1
 
 HEIGHT = 71
 WIDTH = 71
-# HEIGHT = 7
-# WIDTH = 7
 
-x = 0
-y = 0
+START = (0, 0)
+GOAL = (WIDTH - 1, HEIGHT - 1)
 
-goal_x = WIDTH - 1
-goal_y = HEIGHT - 1
+is_open = lambda position, t: blocks[position] > t
 
-def visualize():
-    for y in range(HEIGHT):
-        for x in range(WIDTH):
-            print('.' if grid[(x, y)] else '#', end='')
-        print()
-    print()
-
-# visualize()
-
-DIRECTIONS = (
-    # e s w n
-    (1, 0),
-    (0, 1),
-    (-1, 0),
-    (0, -1)
-)
-
-
-def get_neighbors(position, grid):
+def get_neighbors(position, t):
     x, y = position
 
-    for x_offset, y_offset in DIRECTIONS:
+    for x_offset, y_offset in (
+        ( 1,  0),
+        ( 0,  1),
+        (-1,  0),
+        ( 0, -1)
+    ):
         neighbor_x = x + x_offset
         neighbor_y = y + y_offset
-        neighbor = (neighbor_x, neighbor_y)
 
         if (
             neighbor_x in range(WIDTH)
             and neighbor_y in range(HEIGHT)
-            and grid[neighbor]
+            and is_open(
+                (neighbor_x, neighbor_y),
+                t
+            )
         ):
-            yield neighbor
+            yield (neighbor_x, neighbor_y)
 
-def find_path(grid):
+def find_path(t):
+    distances_from_start = defaultdict(lambda: inf, { START : 0 })
+
     unvisited = set(
         (x, y)
         for x in range(WIDTH)
         for y in range(HEIGHT)
-        # for position, is_open in grid.items()
-        if grid[(x, y)]
+        if is_open(
+            (x, y),
+            t
+        )
     )
-
-    distances_from_start = defaultdict(lambda: inf)
-    # distances_from_start[ (x, y) ] = 0
-    distances_from_start[ (0, 0) ] = 0
-    # visited = set()
-
     while unvisited:
-        # if not len(unvisited) % 100: print(len(unvisited))
-
         position = min(unvisited, key=distances_from_start.__getitem__)
-
-        if position == (goal_x, goal_y):
+        if position == GOAL:
             break
 
         current_score = distances_from_start[position]
-        for neighbor in get_neighbors(position, grid):
-            if neighbor not in unvisited:
-                continue
+        for neighbor in get_neighbors(position, t):
+            if neighbor in unvisited:
+                old_score = distances_from_start[neighbor]
+                new_score = current_score + 1
 
-            old_score = distances_from_start[neighbor]
-            new_score = current_score + 1
+                if new_score < old_score:
+                    distances_from_start[neighbor] = new_score
 
-            if new_score < old_score:
-                distances_from_start[neighbor] = new_score
-
-        # visited.add(position)
         unvisited.remove(position)
 
-    # print(visited)
-    # print(unvisited)
+    return distances_from_start[GOAL]
 
-    return distances_from_start[(goal_x, goal_y)]
+t -= 1
+while find_path(t) == inf:
+    t -= 1
 
-t = 0
-grid = defaultdict(lambda: True)
-lines = lines_original[ : ]
-while True:
-    t += 1
-
-    block, *lines = lines
-    grid[block] = False
-
-    if t < 2900: continue
-
-    print(t)
-    # print(grid)
-
-    path = find_path(grid)
-    print(path)
-    print()
-
-    if path == inf:
-        break
-
-print(t)
-print(block)
+print(
+    '{},{}'.format(
+        *next(
+            block
+            for block, block_t in blocks.items()
+            if block_t == t + 1
+        )
+    )
+)
