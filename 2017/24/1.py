32d878009119a52c51e8c094a4f92efd3fd09767
nibley <5225323+nibley@users.noreply.github.com>
2024-12-23 15:03:43 -0700

    2017 24 (had been skipped)

---
+++ 2017/24/1.py
@@ -0,0 +1,43 @@
+parts = []
+while True:
+    try:
+        line = input()
+    except EOFError:
+        break
+
+    parts.append(tuple(map(int, line.split('/'))))
+
+def get_other_end(free_end, part):
+    return (
+        part[1] if part[0] == free_end
+        else part[0] if part[1] == free_end
+        else None
+    )
+
+def get_bridges(free_end, used=None):
+    if used is None:
+        used = frozenset()
+
+    yield sum(
+        end
+        for part in map(parts.__getitem__, used)
+        for end in part
+    )
+
+    for i, part in enumerate(parts):
+        if i not in used:
+            other_end = get_other_end(free_end, part)
+            if other_end is not None:
+                yield from get_bridges(other_end, used | {i})
+
+print(
+    max(
+        score
+        for starting_part_index, starting_part in enumerate(parts)
+        for score in get_bridges(
+            get_other_end(0, starting_part),
+            frozenset({starting_part_index})
+        )
+        if 0 in starting_part
+    )
+)
---
+++ 2017/24/2.py
@@ -0,0 +1,50 @@
+parts = []
+while True:
+    try:
+        line = input()
+    except EOFError:
+        break
+
+    parts.append(tuple(map(int, line.split('/'))))
+
+def get_other_end(free_end, part):
+    return (
+        part[1] if part[0] == free_end
+        else part[0] if part[1] == free_end
+        else None
+    )
+
+def get_bridges(free_end, used=None):
+    if used is None:
+        used = frozenset()
+
+    yield used
+
+    for i, part in enumerate(parts):
+        if i not in used:
+            other_end = get_other_end(free_end, part)
+            if other_end is not None:
+                yield from get_bridges(other_end, used | {i})
+
+bridges = {}
+for starting_part_index, starting_part in enumerate(parts):
+    if 0 not in starting_part:
+        continue
+
+    for bridge in get_bridges(
+        get_other_end(0, starting_part),
+        frozenset({starting_part_index})
+    ):
+        bridges.setdefault(len(bridge), set()).add(bridge)
+
+longest_bridges = bridges[ max(bridges) ]
+print(
+    max(
+        sum(
+            end
+            for part in map(parts.__getitem__, bridge)
+            for end in part
+        )
+        for bridge in longest_bridges
+    )
+)
