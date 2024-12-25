7bdd5c20eb89eb1ee166b10779e29eb360945d38
nibley <5225323+nibley@users.noreply.github.com>
2024-12-24 23:47:31 -0700

    clean up 2024 25

--- 2024/25/1.py
+++ 2024/25/1.py
@@ -1,68 +1,39 @@
 from itertools import product as cartesian_product
 
-grids = []
-grid = []
+HEIGHT = 7
+
+locks = []
+keys = []
 while True:
+    grid = []
+
     try:
         line = input()
-        if line:
-            grid.append(tuple(line))
-        else:
-            grids.append(tuple(grid))
-            grid = []
     except EOFError:
         break
-    finally:
-        pass
 
-grids.append(tuple(grid))
+    while line:
+        grid.append(line)
 
-def visualize(grid):
-    for row in grid:
-        print(''.join(row))
-    print()
+        try:
+            line = input()
+        except EOFError:
+            line = ''
 
-locks = tuple(
-    grid
-    for grid in grids
-    if all( cell == '#' for cell in grid[0] )
-)
+    assert len(grid) == HEIGHT
+    heights = tuple( column.count('#') for column in zip(*grid) )
 
-keys = tuple(
-    grid
-    for grid in grids
-    if all( cell == '.' for cell in grid[0] )
-)
-
-print(len(locks), 'locks')
-print(len(keys), 'keys')
-
-def valid(lock, key):
-    assert len(lock[0]) == len(key[0])
-    total_height = len(lock)
-
-    for x in range(len(lock[0])):
-        y = 0
-        while y < total_height and lock[y][x] == '#':
-            y += 1
-
-        lock_height = y - 1
-
-        y = total_height - 1
-        while y > 0 and key[y][x] == '#':
-            # print('    key', y)
-            y -= 1
-
-        key_height = y + 1
-
-        if not key_height > lock_height:
-            return False
-
-    return True
+    if grid[0][0] == '#':
+        locks.append(heights)
+    else:
+        keys.append(heights)
 
 print(
     sum(
-        valid(lock, key)
+        all(
+            lock_height + key_height <= HEIGHT
+            for lock_height, key_height in zip(lock, key)
+        )
         for lock, key in cartesian_product(locks, keys)
     )
 )
