cbe33df3b95b6414159d9d1240e26c00a7fac660
nibley <5225323+nibley@users.noreply.github.com>
2025-12-01 22:22:33 -0700

    2025 2

--- 2025/2/1.py
+++ 2025/2/1.py
@@ -1,6 +1,20 @@
-while True:
-    try:
-        line = input()
-    except EOFError:
-        break
+line = input()
+ranges = []
+for raw_range in line.split(','):
+    new_range = tuple(map(int, raw_range.split('-')) )
+    assert len(new_range) == 2 and new_range[1] > new_range[0]
+    ranges.append(new_range)
 
+total = 0
+
+for start, stop in ranges:
+    for i in range(start, stop + 1):
+        text = str(i)
+        
+        if len(text) % 2:
+            continue
+        
+        if text[ : len(text) // 2 ] == text[ len(text) //    2 : ]:
+            total += i
+
+print(total)
---
+++ 2025/2/2.py
@@ -0,0 +1,37 @@
+line = input()
+ranges = []
+for raw_range in line.split(','):
+    new_range = tuple(map(int, raw_range.split('-')) )
+    assert len(new_range) == 2 and new_range[1] > new_range[0]
+    ranges.append(new_range)
+
+total = 0
+
+def is_valid(i):
+    text = str(i)
+    text_length = len(text)
+    
+    for segment_length in range(1, text_length // 2 + 1):
+        if text_length % segment_length:
+            continue
+
+        segment = text[ : segment_length ]
+    
+        found_repeating = True
+        for segment_number in range(1, text_length // segment_length):
+            next_segment = text[ segment_length * segment_number : segment_length * (segment_number + 1) ]
+            if segment != next_segment:
+                found_repeating = False
+                break
+        
+        if found_repeating:
+            return False
+
+    return True
+
+for start, stop in ranges:
+    for i in range(start, stop + 1):
+        if not is_valid(i):
+            total += i
+        
+print(total)
