b43ae04e84f8a8b78eef5f0d5816708c8e64fe13
nibley <5225323+nibley@users.noreply.github.com>
2025-12-02 22:19:22 -0700

    2025 3 (part 1)

--- 2025/3/1.py
+++ 2025/3/1.py
@@ -1,6 +1,28 @@
+import itertools
+import math
+
+banks = []
+
 while True:
     try:
         line = input()
+        bank = tuple(map(int, line))
+        banks.append(bank)
     except EOFError:
         break
 
+def get_max_for_bank(bank):
+    max_score = -1 * math.inf
+    for i, first_value in enumerate(bank):
+        for second_value in bank[ i + 1 : ]:
+            bank_score = 10 * first_value + second_value
+            if bank_score > max_score:
+                max_score = bank_score
+    
+    return max_score
+
+total = 0
+for bank in banks:
+    total += get_max_for_bank(bank)
+
+print(total)
