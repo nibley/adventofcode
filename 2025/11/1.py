30a9440669db0a89d2b8ae064ade8c8699810bca
nibley <5225323+nibley@users.noreply.github.com>
2025-12-10 22:58:20 -0700

    2025 11 (part 1)

--- 2025/11/1.py
+++ 2025/11/1.py
@@ -1,6 +1,23 @@
+connections = {}
+
 while True:
     try:
         line = input()
     except EOFError:
         break
 
+    server, *outputs = line.replace(':', '').split()
+    connections[server] = tuple(outputs)
+
+paths = [ ('you', ) ]
+total = 0
+while paths:
+    current_path = paths.pop()
+    current_server = current_path[-1]
+    for output in connections[current_server]:
+        if output == 'out':
+            total += 1
+        else:
+            paths.append(current_path + (output, ))
+
+print(total)
